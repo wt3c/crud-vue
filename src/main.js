7bf1d303a08d28cd77c19833ab0576c6dc59cef5
Welington Carlos <wcarlos3@gmail.com>
2018-10-05 19:04:29 -0300

    Inicio

---
+++ src/main.js
@@ -0,0 +1,118 @@
+var products = [
+  {id: 1, name: 'Angular', description: 'Superheroic JavaScript MVW Framework.', price: 100},
+  {id: 2, name: 'Ember', description: 'A framework for creating ambitious web applications.', price: 100},
+  {id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100}
+];
+
+
+// Porra mais simples impossivel
+// Gostei muito dessa solução !
+function findProduct(productId) {
+  return products[findProductKey(productId)];
+};
+
+function findProductKey(productId) {
+  for (var key = 0; key < products.length; key++) {
+    if (products[key].id == productId) {
+      return key;
+    }
+  }
+};
+
+var List = Vue.extend({
+  template: '#product-list',
+  data: function () {
+    return {products: products, searchKey: ''};
+  },
+  computed: {
+    filteredProducts() {
+      return this.products.filter((product) => {
+        return product.name.indexOf(this.searchKey) > -1
+        //return !product.name.indexOf(this.searchKey)
+      })
+    }
+  }
+});
+
+var Product = Vue.extend({
+  template: '#product',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  }
+});
+
+var ProductEdit = Vue.extend({
+  template: '#product-edit',
+  data: function () {
+
+    console.log(this.$route.params)
+    // return {product: {name: 'TESTE', description: 'PQP', price: '1000'}};
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+
+  methods: {
+    updateProduct: function () {
+      //Obsolete, product is available directly from data...
+      let product = this.product; //var product = this.$get('product');
+      products[findProductKey(product.id)] = {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price
+      };
+      router.push('/');
+    }
+  }
+});
+
+var ProductDelete = Vue.extend({
+  template: '#product-delete',
+  data: function () {
+    return {product: findProduct(this.$route.params.product_id)};
+  },
+  methods: {
+    deleteProduct: function () {
+      products.splice(findProductKey(this.$route.params.product_id), 1);
+      router.push('/');
+    }
+  }
+});
+
+var AddProduct = Vue.extend({
+  template: '#add-product',
+  data: function () {
+    return {
+      product: {name: '', description: '', price: ''}
+    }
+  },
+  methods: {
+    createProduct: function () {
+      //Obsolete, product is available directly from data...
+      let product = this.product; //var product = this.$get('product');
+      products.push({
+        id: Math.random().toString().split('.')[1],
+        name: product.name,
+        description: product.description,
+        price: product.price
+      });
+      router.push('/');
+    }
+  }
+});
+
+var router = new VueRouter({
+  routes: [
+    {path: '/', component: List},
+    {path: '/product/:product_id', component: Product, name: 'product'},
+    {path: '/add-product', component: AddProduct},
+    {path: '/product/:product_id/edit', component: ProductEdit, name: 'product-edit'},
+    {path: '/product/:product_id/delete', component: ProductDelete, name: 'product-delete'}
+  ]
+});
+
+
+var App = {}
+
+new Vue({
+  router
+}).$mount('#app')
