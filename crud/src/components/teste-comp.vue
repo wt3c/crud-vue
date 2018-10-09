83672b55bef812dfb1fd4265907415ab7b0f5c41
Welington Carlos <wcarlos3@gmail.com>
2018-10-09 13:53:57 -0300

    Morrendo com as importações dos componentes

---
+++ crud/src/components/teste-comp.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "teste-comp",
+      data(){
+          return {
+            msg: 'Teste Boladão'
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
