0f090d6f5074fc784b0d4399d68cd763edee4f04
Welington Carlos <wcarlos3@gmail.com>
2018-11-12 19:56:14 -0200

    Começando com PounchDB

--- crud_quasar/src/store/product-store/state.js
+++ crud_quasar/src/store/product-store/state.js
@@ -1,19 +1,70 @@
-/* eslint-disable consistent-return */
+/* eslint-disable consistent-return,no-undef,no-unused-vars */
+
 /* eslint-disable no-plusplus */
+
+// import PouchDB from pouchdb;
+
+function UsingPouchDb () {
+  var db = new PouchDB('productDB');
+  var remoteCouch = false;
+
+  var products = [
+    {
+      _id: '1',
+      name: 'Angular',
+      description: 'Superheroic JavaScript MVW Framework.',
+      price: 100
+    },
+    {
+      _id: '2',
+      name: 'Ember',
+      description: 'A framework for creating ambitious web applications.',
+      price: 100
+    },
+    {
+      _id: '3',
+      name: 'React',
+      description: 'A JavaScript Library for building user interfaces.',
+      price: 100
+    }
+  ];
+
+  products.forEach(function (prod) {
+    db.put(prod, function callback (err, result) {
+      if (!err) {
+        console.log('Banco criado com sucesso!');
+      } else {
+        console.log('Deu MERDA!!!!!!!!!!!!!!!!!!!!');
+        console.log(err);
+      }
+    }
+    );
+  });
+}
+
+UsingPouchDb();
 export default {
 
   product_list: [
     {
-      id: 1, name: 'Angular', description: 'Superheroic JavaScript MVW Framework.', price: 100
+      id: 1,
+      name: 'Angular',
+      description: 'Superheroic JavaScript MVW Framework.',
+      price: 100
     },
     {
-      id: 2, name: 'Ember', description: 'A framework for creating ambitious web applications.', price: 100
+      id: 2,
+      name: 'Ember',
+      description: 'A framework for creating ambitious web applications.',
+      price: 100
     },
     {
-      id: 3, name: 'React', description: 'A JavaScript Library for building user interfaces.', price: 100
+      id: 3,
+      name: 'React',
+      description: 'A JavaScript Library for building user interfaces.',
+      price: 100
     }
   ],
-
   findProduct (productId) {
     return this.product_list[this.findProductKey(productId)];
   },
